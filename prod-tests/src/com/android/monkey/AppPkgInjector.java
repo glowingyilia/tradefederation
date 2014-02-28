e803a08ec7de8d1b2e653412689c15460bf4d19e
Brett Chabot <brettchabot@google.com>
2014-02-28 11:22:21 -0800

    Treat aapt parse failures as BuildErrors.
    Done so TF doesn't keep retrying a build that fails aapt parse.
    
    Change-Id: Ie403d80749705e5dc4770ba872a32dd658018025

--- prod-tests/src/com/android/monkey/AppPkgInjector.java
+++ prod-tests/src/com/android/monkey/AppPkgInjector.java
@@ -63,12 +63,13 @@ public class AppPkgInjector implements ITargetPreparer, IConfigurationReceiver {
         for (VersionedFile apkFile : appBuild.getAppPackageFiles()) {
             AaptParser aapt = AaptParser.parse(apkFile.getFile());
             if (aapt == null) {
-                // TODO: should this be BuildError?
-                throw new TargetSetupError(String.format("aapt parse of %s failed",
+                // throw a build error because typically aapt parse errors are issues with the apk
+                throw new BuildError(String.format("aapt parse of %s failed",
                         apkFile.getFile().getAbsolutePath()));
             }
             String pkgName = aapt.getPackageName();
             if (pkgName == null) {
+                // this should never happen
                 throw new TargetSetupError(String.format("Failed to parse package name from %s",
                         apkFile.getFile().getAbsolutePath()));
             }
